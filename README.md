433a971e3ad1bbd89225ffcb7de7e879b893825d
Joe D'Alessandro <joe@thejoeshow.net>
2023-12-17 20:55:45 -0500

    updated some colors and refactored  * chnaged the decorate hook to possibly handle multiple changes   * moved color construction to separate function    * set color constants (inspired by snazzy)

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # hyper-joe
 
-applies my favorite colors (at the moment) to [hyper](https://hyper.is/).
+applies adaptive colors to [hyper](https://hyper.is/).
 
+the colors are pulled from the active desktop theme....hopefully
 ## How to use
 
 Add `hyper-joe` to `plugins` in `~/.hyper.js`.
--- index.js
+++ index.js
@@ -1,15 +1,55 @@
+'use strict';
+const foregroundColor = '#f7f5ba';
+const backgroundColor = '#121609';
+const lighterBackgroundColor = '#2e3817';
+const borderColor = 'rgba(251,0,152,0.5)';
+const cursorColor = '#ffbc30';
+const cursorAccentColor = backgroundColor;
+const selectionColor = 'rgba(251,0,152,0.4)';
+const red = '#fe7002';
+const green = '#2fd52c';
+const yellow = '#e1d152';
+const blue = '#57c7ff';
+const magenta = '#945dc3';
+const cyan = '#49a2a3';
+const white = '#fdfcea';
+
+const getColors = config => {
+  // TODO: make color changes based on config here
+  const colors = {
+    backgroundColor,
+    foregroundColor,
+    borderColor,
+    cursorColor,
+    cursorAccentColor,
+    selectionColor,
+    colors: {
+      black: backgroundColor,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      white,
+      lightBlack: lighterBackgroundColor,
+      lightRed: red,
+      lightGreen: green,
+      lightYellow: yellow,
+      lightBlue: blue,
+      lightMagenta: magenta,
+      lightCyan: cyan,
+      lightWhite: foregroundColor
+    }
+  };
+
+  return colors;
+}
+
 exports.decorateConfig = (config) => {
-  return Object.assign({}, config, {
-    borderColor: 'yellow',
-    cursorColor: 'yellow',
-    css: `
-      ${config.css || ''}
-      .tabs_nav .tabs_list .tab_text {
-        color: yellow;
-      }
-      .tabs_nav .tabs_title {
-        color: yellow;
-      }
-    `
-  });
+  const colors = getColors(config);
+
+  // TODO: any other config changes here
+
+  return Object.assign({}, config, colors);
 }
